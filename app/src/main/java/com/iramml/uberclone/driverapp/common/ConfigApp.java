3c7fddf2dad2f266a1df45cf8a7386b50f7d4a44
zulqarnain4 <zulqarnainharun4@gmail.com>
2021-10-07 18:21:06 +0100

    Initial commit

---
+++ app/src/main/java/com/iramml/uberclone/driverapp/common/ConfigApp.java
@@ -0,0 +1,10 @@
+package com.iramml.uberclone.driverapp.common;
+
+public class ConfigApp {
+    public static final double baseFare = 2.55;
+    public static final double timeRate = 0.35;
+    public static final double distanceRate = 1.75;
+    public static final String CLOUD_MESSAGING_SERVER_GEY = "AAAAaiphq8A:APA91bGiTklTbdwbBpSG3NUTKxp7_469kWQ8Oj0RB-OkUHaPsEjZ0KyUPjciA00eykY7knJK14oymlexLsB1hPvfTOIdZum4kyazBJKTg53hcY0JFzFCRV_O202jYK3Gth_BRfyj-opb";
+    public static final String GOOGLE_API_KEY = "AIzaSyBsk80Uh9Zag5pFu-fvMq7qkq1JF1Y8EVg";
+
+}
